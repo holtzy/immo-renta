a02d15b184e62a3082647107b3401ae1be149632
holtzy <yan.holtz@datadoghq.com>
2021-03-25 19:21:29 -0400

    better explanation modal

--- src/components/ExplanationModals.tsx
+++ src/components/ExplanationModals.tsx
@@ -14,10 +14,15 @@ export const RentabiliteBruteExplanationModal = () => {
 
             <Modal show={isOpen} onHide={() => setIsOpen(false)}>
                 <Modal.Body>
-                    <h2>Rentabilité brute</h2>
+                    <h2><span>🤔 </span>Rentabilité brute</h2>
                     <p>
-                        Some explanation about what's the rentabilité brute
+                        Le rendement brut est le plus simple à calculer. Il ne prend pas en compte des paramètres
+                        pourtant essentiels comme les charges payées par le locataires ou la fiscalité.
                     </p>
+                    <p>Il se calcule en divisant le loyer annuel par le prix du bien, multiplié
+                        par 100 pour obtenir un pourcentage.</p>
+                    <p>Ici on obtient donc... formule.</p>
+                    <p><b>Attention</b></p>
                 </Modal.Body>
             </Modal>
         </>

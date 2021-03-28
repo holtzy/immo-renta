7c60b42e6abcfb8c25f52b23c334ca46632f54a0
holtzy <yan.holtz@datadoghq.com>
2021-03-27 21:55:20 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,25 @@
 module.exports = {
-    plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-less`]
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "UA-79254642-2", // Google Analytics / GA
+                ],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                gtagConfig: {
+                    anonymize_ip: false,
+                },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: true,
+                },
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-less`
+    ]
 }
